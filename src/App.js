8d2c1e81bcb5e212c9aa66fdb51e23d439a997d6
bradwhs <52989420+bradwhs@users.noreply.github.com>
2020-07-31 23:13:25 +0900

    deploy

--- src/App.js
+++ src/App.js
@@ -92,7 +92,7 @@ class App extends Component {
 
   onButtonSubmit = () => {
     this.setState({imageUrl: this.state.input})
-      fetch('http://localhost:3000/imageUrl', {
+      fetch('https://secure-wildwood-98935.herokuapp.com/imageUrl', {
         method: 'post',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
@@ -102,7 +102,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://secure-wildwood-98935.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
